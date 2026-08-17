db9afc1f19357fdac17da290a4dccdfeeaafcf28
FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>
2026-08-17 12:01:58 +0100

    Fixed migration to remove duplicate tempalte keys

--- src/GovUK.Dfe.FlexForms.Infrastructure/Migrations/20260817120000_AddUniqueTemplateNamePerTenant.cs
+++ src/GovUK.Dfe.FlexForms.Infrastructure/Migrations/20260817120000_AddUniqueTemplateNamePerTenant.cs
@@ -12,6 +12,63 @@ public partial class AddUniqueTemplateNamePerTenant : Migration
 {
     protected override void Up(MigrationBuilder migrationBuilder)
     {
+        // Unique indexes treat trailing spaces as equal and typically use CI collation,
+        // so "default" / "Default" / "default " all collide. Rename later duplicates
+        // before creating the index.
+        migrationBuilder.Sql("""
+            UPDATE ea.Templates
+            SET Name = RTRIM(Name)
+            WHERE TenantId IS NOT NULL
+              AND Name <> RTRIM(Name);
+
+            DECLARE @id uniqueidentifier;
+            DECLARE @tenant uniqueidentifier;
+            DECLARE @base nvarchar(100);
+            DECLARE @new nvarchar(100);
+            DECLARE @n int;
+
+            WHILE 1 = 1
+            BEGIN
+                SET @id = NULL;
+
+                SELECT TOP (1)
+                    @id = extra.TemplateId,
+                    @tenant = extra.TenantId,
+                    @base = RTRIM(extra.Name)
+                FROM ea.Templates extra
+                INNER JOIN ea.Templates keeper
+                    ON keeper.TenantId = extra.TenantId
+                   AND keeper.TemplateId <> extra.TemplateId
+                   AND LOWER(RTRIM(keeper.Name)) = LOWER(RTRIM(extra.Name))
+                   AND (
+                        keeper.CreatedOn < extra.CreatedOn
+                        OR (keeper.CreatedOn = extra.CreatedOn AND keeper.TemplateId < extra.TemplateId)
+                   )
+                WHERE extra.TenantId IS NOT NULL
+                ORDER BY extra.CreatedOn DESC, extra.TemplateId DESC;
+
+                IF @id IS NULL
+                    BREAK;
+
+                SET @n = 1;
+                SET @new = LEFT(@base, 99) + N'1';
+
+                WHILE EXISTS (
+                    SELECT 1
+                    FROM ea.Templates
+                    WHERE TenantId = @tenant
+                      AND LOWER(RTRIM(Name)) = LOWER(RTRIM(@new)))
+                BEGIN
+                    SET @n = @n + 1;
+                    SET @new = LEFT(@base, 100 - LEN(CAST(@n AS varchar(10)))) + CAST(@n AS varchar(10));
+                END
+
+                UPDATE ea.Templates
+                SET Name = @new
+                WHERE TemplateId = @id;
+            END
+            """);
+
         migrationBuilder.CreateIndex(
             name: "IX_Templates_TenantId_Name",
             schema: "ea",
